0bc2d3683094f9d8f029a5696df276a39cd85288
Ramiro <ramifazio@gmail.com>
2022-08-03 12:28:43 -0300

    Sugerencias

--- main.js
+++ main.js
@@ -1,37 +1,47 @@
 
 let cadena_descifrar = prompt("que quieres descifrar ?");
 
-let arreglo_cadena=Array.from(cadena_descifrar);
+let arreglo_cadena = Array.from(cadena_descifrar);
 
-let cadena_final=new Array();
+let cadena_final = new Array();
 
-let total=0;
+let total = 0;
 
-for(let i=0; i<arreglo_cadena.length; i++){
-  
-  if(arreglo_cadena[i]=="."){
+for (let i = 0; i < arreglo_cadena.length; i++) {
+
+  if (arreglo_cadena[i] === ".") {
     cadena_final.push(1);
   }
-  if (arreglo_cadena[i] == ",") {
+  if (arreglo_cadena[i] === ",") {
     cadena_final.push(5);
   }
-  if (arreglo_cadena[i] == ":") {
+  if (arreglo_cadena[i] === ":") {
     cadena_final.push(10);
   }
-  if (arreglo_cadena[i] == ";") {
+  if (arreglo_cadena[i] === ";") {
     cadena_final.push(50);
   }
-  if (arreglo_cadena[i] == "!") {
+  if (arreglo_cadena[i] === "!") {
     cadena_final.push(100);
   }
+
+  /*
+    recomiendo usar "===" para hacer mejores comparaciones y buenas practicas!
+  */
 
 }
 console.log(cadena_final);
 
-cadena_final = cadena_final.map((x) => parseInt(x,10));
+cadena_final = cadena_final.map((x) => parseInt(x, 10));
 
 total = cadena_final.reduce(function (a, b) {
   return a + b;
 });
 
 console.log(total);
+
+
+/*
+  Intente buscar otra logica para darte otra forma de hacerlo, hay muchas! 
+  Pero esta es la mejor... Bien ahi! Me gusta, divertido y buena logica
+*/
